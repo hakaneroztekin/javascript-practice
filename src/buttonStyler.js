3510cd3e7da4509d0db730e69bd529a9ab379bdb
eroztekin <eroztekin@itu.edu.tr>
2019-08-31 14:05:13 +0300

    add external script

---
+++ src/buttonStyler.js
@@ -0,0 +1,10 @@
+let elasticStyle = document.getElementById('elasticLine').style;
+function makeFontBig() {
+    elasticStyle.fontSize = '35px'
+}
+function makeFontVerySmall() {
+    elasticStyle.fontSize = '2px'
+}
+function makeItDisappear() {
+    elasticStyle.display = 'none'
+}
