01dd7ee992588194dacf95abcf73d9d276064ac9
eroztekin <eroztekin@itu.edu.tr>
2019-08-31 18:18:42 +0300

    switch case in a button to simulate a basic RPG game :)

--- src/events.js
+++ src/events.js
@@ -1,3 +1,21 @@
+let clickCounter = 1;
+
 function changeText(id) {
-    document.getElementById(id).innerText = "Hello there!";
+    document.getElementById(id).innerText = printText();
+    clickCounter++;
+}
+
+function printText() {
+    switch(clickCounter) {
+        case 1:
+            return "Hello there!";
+        case 2:
+            return "The taverna is about to be closed.";
+        case 3:
+            return "See you!";
+        case 4:
+            clickCounter = 0;
+            return "Start again?";
+    }
+
 }
